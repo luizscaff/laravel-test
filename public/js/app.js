0f7a5d222e06f30506aff68c6185fb1e47f2f5cf
Luiz Scaff <luiz.gmscaff@gmail.com>
2022-10-02 20:41:27 -0300

    Adicionado "select2"

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+(function()
+{
+  //...........................................................
+  // Select2
+  $('.searchable-select').select2(
+  {
+    width: "resolve",
+    placeholder: {
+      id: -1, // the value of the option
+      text: "Selecione uma opção"
+    },
+    allowClear: true,
+  });
+
+  $('.multiple-searchable-select').select2(
+  {
+    width: "resolve",
+    placeholder: {
+      id: -1, // the value of the option
+      text: "Selecione uma opção"
+    },
+    allowClear: true,
+    multiple: true,
+  });
+
+});
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,16 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <!-- Styles -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" rel="stylesheet" />
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
         <!-- Scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
